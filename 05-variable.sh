e04bd0696acffbffb9e50ed6143b94797effa30d
Kishore Eligeti <kishoreligeti30>
2026-06-09 22:38:58 +0530

    Conversation added

---
+++ 05-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2 #no space before and after equal
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2 : Hi $PERSON1, How are you"
+echo "$PERSON1 : I'm fine, How are you"
+echo "$PERSON2 : Goood !!!"
